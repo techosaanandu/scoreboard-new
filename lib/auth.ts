9d5bc4f8df141cc0513bd7c1fda26abffce82a01
techosaanandu <techosaanandu@gmail.com>
2026-01-17 17:25:14 +0530

    new4

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,4 @@
 import { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthOptions = {
